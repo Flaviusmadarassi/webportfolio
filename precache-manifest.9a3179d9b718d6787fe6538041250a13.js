af566c657c3428c9183563041e1ad60f95e3a4c3
Flaviusmadarassi <flavius.madarassi@yahoo.com>
2020-07-06 00:39:12 +0300

    Updates

---
+++ precache-manifest.9a3179d9b718d6787fe6538041250a13.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5c5e9ce77806f1de54f328ae32dc5b",
+    "url": "/webportfolio/index.html"
+  },
+  {
+    "revision": "12338c1c6f6cadd590c6",
+    "url": "/webportfolio/static/css/main.68371f9e.chunk.css"
+  },
+  {
+    "revision": "7a8499588632b85fd444",
+    "url": "/webportfolio/static/js/2.ee30a7e1.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/webportfolio/static/js/2.ee30a7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12338c1c6f6cadd590c6",
+    "url": "/webportfolio/static/js/main.39e2160a.chunk.js"
+  },
+  {
+    "revision": "853f39997dd8cdd5c73c",
+    "url": "/webportfolio/static/js/runtime-main.da1a3077.js"
+  },
+  {
+    "revision": "d504b71f1c46f8f4bcaac1535bed2a9a",
+    "url": "/webportfolio/static/media/clouds2.d504b71f.jpg"
+  },
+  {
+    "revision": "9a359a75f68b18802df2874fe355b8d4",
+    "url": "/webportfolio/static/media/email.9a359a75.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/webportfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "c0b8c2e82943aad9ce4f29386fac3af1",
+    "url": "/webportfolio/static/media/myIMDb1.c0b8c2e8.png"
+  },
+  {
+    "revision": "833bda2eeaa2b8e60b93a3901e48666c",
+    "url": "/webportfolio/static/media/myIMDb2.833bda2e.png"
+  },
+  {
+    "revision": "b186a376340391eec3603b33d821bce9",
+    "url": "/webportfolio/static/media/myIMDb3.b186a376.png"
+  },
+  {
+    "revision": "2d008dcd61ecc3852a60e8b6d9d0c30f",
+    "url": "/webportfolio/static/media/myIMDb4.2d008dcd.png"
+  },
+  {
+    "revision": "1fcffd9b35598588144c5a553203d359",
+    "url": "/webportfolio/static/media/myIMDb5.1fcffd9b.png"
+  },
+  {
+    "revision": "f81a6f373c244b1f70f4b7402b5ab372",
+    "url": "/webportfolio/static/media/react.f81a6f37.png"
+  },
+  {
+    "revision": "f161d2ef4aad40d2e5e50d8498196440",
+    "url": "/webportfolio/static/media/scoalaIT.f161d2ef.jpg"
+  }
+]);
